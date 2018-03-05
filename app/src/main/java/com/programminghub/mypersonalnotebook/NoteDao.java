bb3c431300639c0d6aca98b91b0a35ffbc0630c5
BahulaKaji <nilesh.katuwal@gmail.com>
2018-03-05 11:59:34 +0545

    part one

---
+++ app/src/main/java/com/programminghub/mypersonalnotebook/NoteDao.java
@@ -0,0 +1,30 @@
+package com.programminghub.mypersonalnotebook;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Delete;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
+import java.util.List;
+
+/**
+ * Created by rojin on 3/4/2018.
+ */
+
+@Dao
+public interface NoteDao {
+
+    @Insert
+    void insertNote(Note note);
+
+    @Update
+    void updateNote(Note note);
+
+    @Delete
+    void deleteNote(Note note);
+
+    @Query("Select * from note")
+    List<Note> getAllNote();
+
+}
